f9fa17f752b6efb1d0165ce708e4126881cedaa8
Paul Reynolds <usuallypaulreynolds@gmail.com>
2022-04-11 12:15:10 -0500

    Finished exceptions exercise

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -16,16 +16,42 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        studentFiles.forEach((key, value) -> {
+
+            if (CheckFileExtension(value) != 1) {
+                System.out.println(key);
+            }
+        });
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
         // Write code here!
+        if (y == 0) try {
+            throw new ArithmeticException("Can't divide by 0!");
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+        return x / y;
     }
 
     public static int CheckFileExtension(String fileName)
     {
         // Write code here!
+        if (fileName.equals("")) try {
+            throw new Exception("BOO");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+        String extension = "";
+        int i =fileName.lastIndexOf('.');
+        if (i > 0) {
+            extension = fileName.substring(i + 1);
+        }
+        if (extension.equals("java")) return 1;
+        return 0;
     }
 
 }
